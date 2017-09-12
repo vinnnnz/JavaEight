fd23f950a9c803b3b80dba16e62a61b8cfb97e3c
vinnnnz <vinnnnz@gmail.com>
2017-09-12 15:46:58 +0530

    factorial of big integer

--- src/main/java/com/vinz/funcitonal/XFactorial.java
+++ src/main/java/com/vinz/funcitonal/XFactorial.java
@@ -1,16 +1,25 @@
 package com.vinz.funcitonal;
 
+import java.math.BigInteger;
 import java.util.function.UnaryOperator;
 
 public class XFactorial {
 	
 	static UnaryOperator<Long> fac = number -> number == 0 ? 1 : number * XFactorial.fac.apply( number - 1);
+	static UnaryOperator<BigInteger> bigfac = number -> number.compareTo(BigInteger.ZERO) == 0 ? BigInteger.ONE : number.multiply(XFactorial.bigfac.apply(number.subtract(BigInteger.ONE)));
 	
 	static Long factorial(Long number) {
 		return fac.apply(number);
 	}
 	
+	static BigInteger factorial(BigInteger number) {
+		return bigfac.apply(number);
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		System.out.println(factorial(35L));
+		System.out.println(factorial(BigInteger.valueOf(100)));
 	}
 }
